91e594d809a4e4aba669f8308516794f485a6811
Dale Roberts <ddlroberts32@gmail.com>
2021-08-02 22:12:54 -0400

    Favorites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -6,6 +6,7 @@ import Directory from "./DirectoryComponent";
 import Constants from "expo-constants";
 import CampsiteInfo from "./CampsiteInfoComponent";
 import Reservation from "./ReservationComponent";
+import Favorites from "./FavoritesComponent";
 import { View, Platform, StyleSheet, Text, ScrollView, Image } from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
 import { createDrawerNavigator, DrawerItems } from "react-navigation-drawer";
@@ -138,6 +139,28 @@ const ReservationNavigator = createStackNavigator(
   }
 );
 
+const FavoritesNavigator = createStackNavigator(
+  {
+    Favorites: { screen: Favorites },
+  },
+  {
+    defaultNavigationOptions: ({navigation}) => ({
+        headerStyle: {
+        backgroundColor: "#5637DD",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+      headerLeft: <Icon
+                name='heart'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()} />
+    })
+  }
+);
+
 const CustomDrawerContentComponent = props => (
     <ScrollView>
         <SafeAreaView
@@ -197,6 +220,19 @@ const MainNavigator = createDrawerNavigator(
           )
       }       
   },
+  Favorites: { 
+    screen: FavoritesNavigator,
+    navigationOptions: {
+        drawerLabel: 'My Favorites',
+        drawerIcon: ({tintColor}) => (
+            <Icon
+                name='heart'
+                type='font-awesome'
+                size={24}
+                color={tintColor} />
+        )
+    }       
+},
     Contact: { 
         screen: ContactNavigator,
         navigationOptions: {
--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = 'http://172.20.4.168:3001/';
+export const baseUrl = 'http://192.168.0.24:3001/';
