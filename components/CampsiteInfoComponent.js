eb9ba59a500a405cc054208cb8232d54bfd207a2
Dale Roberts <ddlroberts32@gmail.com>
2021-08-07 13:07:33 -0400

    Animatable and Gestures2

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -37,6 +37,8 @@ function RenderCampsites(props) {
 
   const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
 
+  const recognizeComment = ({dx}) => (dx > 200) ? true : false;
+
   const panResponder = PanResponder.create({
      onStartShouldSetPanResponder: () => true,
      onPanResponderGrant: () => {
@@ -63,8 +65,14 @@ function RenderCampsites(props) {
            ],
            { cancelable: false}
          );
-       }
+       } else if (recognizeComment(gestureState)) {
+          
+         props.onShowModal();
+
+      }
        return true;
+
+       
      }
   })
 
--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Text, View, ScrollView, StyleSheet,
-    Picker, Switch, Button, Modal } from 'react-native';
+    Picker, Switch, Button, Modal, Alert } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
+import * as Animatable from 'react-native-animatable';
+
 
 class Reservation extends Component {
 
@@ -14,20 +16,36 @@ class Reservation extends Component {
             date: new Date(),
             showCalendar: false,
             showModal: false
+            
         };
     }
+
+    
 
     static navigationOptions = {
         title: 'Reserve Campsite'
     }
 
-    toggleModal() {
-        this.setState({showModal: !this.state.showModal});
-    }
-
     handleReservation() {
         console.log(JSON.stringify(this.state));
-        this.toggleModal();
+        Alert.alert(
+            "Begin Search",
+            "Number of Campers " + this.state.campers + "\n Hike in? " + this.state.hikeIn + "\n Date " + 
+            this.state.date.toLocaleDateString('en-US')
+              ,
+            [
+              {
+                text: "Cancel",
+                 onPress: () => this.resetForm(),
+                style: "cancel",
+              },
+              {
+                text: "OK",
+                onPress: () => this.resetForm(),
+            },
+            ],
+            { cancelable: false }
+          )
     }
     resetForm() {
         this.setState({
@@ -42,6 +60,7 @@ class Reservation extends Component {
     render() {
         return (
             <ScrollView>
+                <Animatable.View animation='zoomIn' duration={2000} delay={1000} >
                 <View style={styles.formRow}>
                     <Text style={styles.formLabel}>Number of Campers</Text>
                     <Picker
@@ -96,28 +115,8 @@ class Reservation extends Component {
                         accessibilityLabel='Tap me to search for available campsites to reserve'
                     />
                 </View>
-                <Modal
-                   animationType={'slide'}
-                   transparent={false}
-                   visible={this.state.showModal}
-                   onRequestClose={() => this.toggleModal()}
-                >
-                    <View style={styles.modal}>
-                        <Text style={styles.modalTitle}>Search Campsite Reservations</Text>
-                        <Text style={styles.modalText}>Number of Campers: {this.state.campers}</Text>
-                        <Text style={styles.modalText}>Hike-In: {this.state.hikeIn ? 'Yes' : 'No'}</Text>
-                        <Text style={styles.modalText}>Date: {this.state.date.toLocaleDateString('en-US')}</Text>
-                        <Button
-                            onPress={() => {
-                                this.toggleModal();
-                                this.resetForm();
-                            }}
-                            color='#5637DD'
-                            title='Close' />
-                    </View>
-
-
-                </Modal>
+                
+                </Animatable.View>
             </ScrollView>
         );
     }
